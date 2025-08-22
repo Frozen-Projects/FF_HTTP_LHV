b1bbe93d80aee4c53c39be9ae3ff5027b78cc426
Frozen Forest Reality Technologies <frozenforeststudio@gmail.com>
2025-08-22 03:25:01 +0300

    Update LibHv.Build.cs

--- Source/LibHv/LibHv.Build.cs
+++ Source/LibHv/LibHv.Build.cs
@@ -7,7 +7,7 @@ namespace UnrealBuildTool.Rules
         public LibHv(ReadOnlyTargetRules Target) : base(Target)
         {
     		Type = ModuleType.External;
-            UndefinedIdentifierWarningLevel = WarningLevel.Off;
+            CppCompileWarningSettings.UndefinedIdentifierWarningLevel = WarningLevel.Off;
             bEnableExceptions = true;
 
             if (Target.Platform == UnrealTargetPlatform.Win64)
